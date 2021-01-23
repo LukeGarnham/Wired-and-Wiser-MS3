b4aecfabb14a20dae488a940872444c5d69b4cae
LukeGarnham <lukegarnham@msn.com>
2021-01-23 19:06:05 +0000

    Created signin page and functionality.

--- app.py
+++ app.py
@@ -25,6 +25,38 @@ def home():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    # Check if the message is POST
+    if request.method == "POST":
+        # Check if email address exists in db
+        existing_user = mongo.db.users.find_one(
+            {"user_email_address": request.form.get(
+                "email").lower()})
+
+        # If it doesn, display a flash message informing user that the an account already exists for this email address.
+        if existing_user:
+            flash("Account already exists for this email address.")
+            return redirect(url_for("register"))
+
+        # Otherwise, assign form inputs (values) to keys and insert record into users collection.
+        record = {
+            "first_name": request.form.get("first-name").lower(),
+            "last_name": request.form.get("last-name").lower(),
+            "user_email_address": request.form.get("email").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(record)
+
+        # Then, put the new users email address into session cookie and display flash success message.
+        session["user"] = request.form.get("email").lower()
+        flash("Registration successful")
+
+    # If method is not POST (i.e. GET) render register.html template.
+    return render_template("register.html")
+
+
+@app.route("/signin", methods=["GET", "POST"])
+def signin():
+    # Check if the message is POST
     if request.method == "POST":
         # Check if username exists in db
         existing_user = mongo.db.users.find_one(
@@ -32,22 +64,25 @@ def register():
                 "email").lower()})
 
         if existing_user:
-            flash("Email address already exists.")
-            return redirect(url_for("register"))
+            # Ensure hashed password matches password input by user
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    # If so, put the new users email address into session cookie and display flash success message.
+                    session["user"] = request.form.get("email").lower()
+                    flash("Welcome back {}".format(
+                        existing_user["first_name"].capitalize()))
+            else:
+                # If password doesn't match, display flash message informing the user and return them to a blank sign in page.
+                flash("Incorrect Email Address and/or Password")
+                return redirect(url_for("signin"))
 
-        register = {
-            "first_name": request.form.get("first-name").lower(),
-            "last_name": request.form.get("last-name").lower(),
-            "user_email_address": request.form.get("email").lower(),
-            "password": generate_password_hash(request.form.get("password"))
-        }
-        mongo.db.users.insert_one(register)
+        # If  email address doesn't exist in db, display flash message informing the user and return them to a blank sign in page.
+        else:
+            flash("Incorrect Email Address and/or Password")
+            return redirect(url_for("signin"))
 
-        # Put the new user into session cookie.
-        session["user"] = request.form.get("email").lower()
-        flash("Registration successful")
-
-    return render_template("register.html")
+    # If method is not POST (i.e. GET) render register.html template.
+    return render_template("signin.html")
 
 
 if __name__ == "__main__":
