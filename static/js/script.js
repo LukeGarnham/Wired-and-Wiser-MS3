260dcdc0dc294b3a7d5fa5341f4cd947e4b18f39
LukeGarnham <lukegarnham@msn.com>
2021-01-31 18:16:24 +0000

    Used jQuery to changed modal effects.

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,3 @@
-console.log("test")
 // ----------------------------------------------------------------------------------------------------------Address Lookup
 // The below code was initially copied from: https://getaddress.io/
 // Free plan = 20 calls/day limit
@@ -21,62 +20,60 @@ console.log("test")
 // Enforce a minimum date on the Booking form date picker.  Earliest booking date must be made 30 days in advance of today.
 // Solution found here:  https://stackoverflow.com/questions/32378590/set-date-input-fields-max-date-to-today
 // And here: https://stackoverflow.com/questions/44827066/add-30-days-to-a-current-date-js
-if (document.getElementById("install_date")) {
-    let today = new Date();
-    today.setDate(today.getDate() + 30)
-    let day = today.getDate();
-    if (day < 10) {
-        day = "0" + day;
-    }
-    let month = today.getMonth()+1;
-    if (month < 10) {
-        month = "0" + month;
-    }
-    let year = today.getFullYear();
-    today = year+"-"+month+"-"+day;
-    document.getElementById("install_date").setAttribute("min", today);
-};
+
+
+// if (document.getElementById("install_date")) {
+//     let today = new Date();
+//     today.setDate(today.getDate() + 30)
+//     let day = today.getDate();
+//     if (day < 10) {
+//         day = "0" + day;
+//     }
+//     let month = today.getMonth()+1;
+//     if (month < 10) {
+//         month = "0" + month;
+//     }
+//     let year = today.getFullYear();
+//     today = year+"-"+month+"-"+day;
+//     document.getElementById("install_date").setAttribute("min", today);
+// };
 
 // ----------------------------------------------------------------------------------------------------------Modals
 
-// ------------------------------------------------Check for an element on the book.html page and run these rules on that page.
-if (document.getElementById("launch-meter-id-modal-1")) {
-// ------------------------------------------------Meter ID Modal
-    document.getElementById("launch-meter-id-modal-1").addEventListener("click", function getElement() {
-        document.getElementById("meter-id-modal").classList.remove("d-none");
+$(document).ready(function() {
+
+    // ------------------------------------------------Meter ID Modal
+    // Open Meter ID modal when icons with class launch-meter-id-modal are clicked.
+    $(".launch-meter-id-modal").click(function() {
+        $("#meter-id-modal").fadeIn();
+    });
+    // Close Meter ID modal when icon with ID close-meter-id-modal is clicked.
+    $("#close-meter-id-modal").click(function() {
+        $("#meter-id-modal").fadeOut();
     });
 
-    document.getElementById("launch-meter-id-modal-2").addEventListener("click", function getElement() {
-        document.getElementById("meter-id-modal").classList.remove("d-none");
+    // ------------------------------------------------Meter Serial Number Modal
+    // Open MSN modal when icons with class launch-msn-modal are clicked.
+    $(".launch-msn-modal").click(function() {
+        $("#msn-modal").fadeIn();
+    });
+    // Close MSN modal when icon with ID close-msn-modal is clicked.
+    $("#close-msn-modal").click(function() {
+        $("#msn-modal").fadeOut();
     });
 
-    document.getElementById("close-meter-id-modal").addEventListener("click", function getElement() {
-        document.getElementById("meter-id-modal").classList.add("d-none");
+    // Set up the install date with a datepicker widget using jQuery UI:  https://api.jqueryui.com/datepicker/
+    // $("#install_date" ).datepicker();
+    // $.datepicker.formatDate("dd/mm/yyyy");
+
+    // ------------------------------------------------Delete Booking Modal
+    // Open Delete Booking modal when button with id launch-delete-modal is clicked.
+    $("#launch-delete-modal").click(function() {
+        $("#delete-booking-modal").fadeIn();
+    });
+    // Close Delete Booking modal when icon with id close-delete-modal is clicked.
+    $("#close-delete-modal").click(function() {
+        $("#delete-booking-modal").fadeOut();
     });
 
-// ------------------------------------------------Meter Serial Number Modal
-    document.getElementById("launch-msn-modal-1").addEventListener("click", function getElement() {
-        document.getElementById("msn-modal").classList.remove("d-none");
-    });
-
-    document.getElementById("launch-msn-modal-2").addEventListener("click", function getElement() {
-        document.getElementById("msn-modal").classList.remove("d-none");
-    });
-
-    document.getElementById("close-msn-modal").addEventListener("click", function getElement() {
-        document.getElementById("msn-modal").classList.add("d-none");
-    });
-};
-
-// ------------------------------------------------Delete Booking Modal
-
-if (document.getElementById("lanch-delete-modal")) {
-    document.getElementById("lanch-delete-modal").addEventListener("click", function getElement() {
-        console.log("Test Delete")
-        document.getElementById("delete-booking-modal").classList.remove("d-none");
-    });
-
-    document.getElementById("close-delete-booking-modal").addEventListener("click", function getElement() {
-        document.getElementById("delete-booking-modal").classList.add("d-none");
-    });
-};
+});
